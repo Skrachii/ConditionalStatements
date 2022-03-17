039b54ae1f899ac7d00206476c57d2d2a33936f3
Skrachi <stefanov.s.n@hotmail.com>
2022-03-17 22:14:07 +0200

    first

---
+++ Demo_1Switch.java
@@ -0,0 +1,36 @@
+package com.telerikacademy.ConditionalStatements;
+
+import java.util.Scanner;
+
+public class Demo_1Switch {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int dayNum = Integer.parseInt(scan.nextLine());
+        switch (dayNum) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Not a valid day");
+
+        }
+    }
+}
